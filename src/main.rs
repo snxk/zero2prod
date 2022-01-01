de669e75e458332f76fe67d3d3804e5e12419465
Sayan Mallick <snyxmk@gmail.com>
2022-01-01 09:11:22 +0530

    added /subscriptions endpoint and implemented tests on subscriptions

--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,7 @@ use zero2prod::run;
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     let listener = std::net::TcpListener::bind("127.0.0.1:0").expect("Failed to bind");
+    let addr = listener.local_addr().expect("Failed to get local address");
+    println!("Listening on {}", addr);
     run(listener)?.await
 }
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -24,3 +24,50 @@ fn spawn_app() -> String {
 
     format!("http://127.0.0.1:{}", port)
 }
+
+// Subscribe Test
+#[actix_rt::test]
+async fn subscriber_returns_a_200_for_valid_form_data() {
+    let address = spawn_app();
+    let client = reqwest::Client::new();
+    let body = "name=le%20lemon&email=le%20lemon%40example.com";
+
+    let response = client
+        .post(&format!("{}/subscriptions", &address))
+        .header("Content-Type", "application/x-www-form-urlencoded")
+        .body(body)
+        .send()
+        .await
+        .expect("Failed to send request");
+
+    assert_eq!(200, response.status().as_u16());
+}
+
+#[actix_rt::test]
+async fn subscriber_returns_a_400_for_invalid_form_data() {
+    let address = spawn_app();
+    let client = reqwest::Client::new();
+
+    let test_cases = vec![
+        ("name=le%20lemon", "missing the email"),
+        ("email=le%20lemon", "missing the name"),
+        ("", "missing both name and email"),
+    ];
+
+    for (invalid_body, error_message) in test_cases {
+        let response = client
+            .post(&format!("{}/subscriptions", &address))
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .body(invalid_body)
+            .send()
+            .await
+            .expect("Failed to execute request");
+
+        assert_eq!(
+            400,
+            response.status().as_u16(),
+            "The API did not fail with 400 Bad Request when the payload was {}.",
+            error_message
+        )
+    }
+}
