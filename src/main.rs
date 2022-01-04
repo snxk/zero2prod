aff768111f10035ec757d58668c0e30e3d6c6af5
Sayan Mallick <snyxmk@gmail.com>
2022-01-04 15:59:43 +0530

    More tracing

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,26 @@
-use env_logger::Env;
 use sqlx::PgPool;
 use std::net::TcpListener;
+use tracing::subscriber::set_global_default;
+use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
+use tracing_log::LogTracer;
+use tracing_subscriber::{layer::SubscriberExt, EnvFilter, Registry};
+
 use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
 
 // TODO - Add CI/CD support for Rust and Postgres
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+    LogTracer::init().expect("Failed to initialize logger");
+
+    let env_filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info"));
+    let formatting_layer = BunyanFormattingLayer::new("zero2prod".into(), std::io::stdout);
+    let subscriber = Registry::default()
+        .with(env_filter)
+        .with(JsonStorageLayer)
+        .with(formatting_layer);
+
+    set_global_default(subscriber).expect("Failed to set global default subscriber");
 
     let config = get_configuration().expect("Failed to load configuration");
     let connection_pool = PgPool::connect(&config.database.connection_string())
@@ -17,5 +30,6 @@ async fn main() -> std::io::Result<()> {
         .expect("Failed to bind");
     let address = listener.local_addr().expect("Failed to get local address");
     println!("Listening on {}", address);
+
     run(listener, connection_pool)?.await
 }
