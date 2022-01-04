0a0248f1e6aa2a97598f41bc7d2be54390951077
Sayan Mallick <snyxmk@gmail.com>
2022-01-04 19:44:04 +0530

    Tracing Completely Implementing

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ use zero2prod::telemetry::{get_subscriber, init_subscriber};
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    let subscriber = get_subscriber("zero2prod".into(), "info".into());
+    let subscriber = get_subscriber("zero2prod".into(), "info".into(), std::io::stdout);
     init_subscriber(subscriber);
 
     let config = get_configuration().expect("Failed to load configuration");
