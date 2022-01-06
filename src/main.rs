c0736b5a9ef7d0747d8f6c04d9d193344ff97e34
Sayan Mallick <snyxmk@gmail.com>
2022-01-06 12:28:22 +0530

    subscribe endpoint completed

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,16 @@
 use axum::{
-    extract::Form,
+    extract::{Extension, Form},
     http::StatusCode,
     routing::{get, post},
-    Router, Server,
+    AddExtensionLayer, Router, Server,
 };
+use chrono::Utc;
 use serde::Deserialize;
+use sqlx::postgres::PgPoolOptions;
+use sqlx::{Pool, Postgres};
 use std::net::SocketAddr;
 use tower_http::trace::TraceLayer;
+use uuid::Uuid;
 
 #[derive(Deserialize, Debug)]
 struct FormData {
@@ -21,9 +25,16 @@ async fn main() {
     }
     tracing_subscriber::fmt::init();
 
+    let db_pool = PgPoolOptions::new()
+        .max_connections(5)
+        .connect("postgres://postgres:password@localhost:5432/newsletter")
+        .await
+        .expect("Failed to connect to database");
+
     let app = Router::new()
         .route("/health_check", get(health_check))
         .route("/subscriptions", post(subscribe))
+        .layer(AddExtensionLayer::new(db_pool))
         .layer(TraceLayer::new_for_http());
 
     let address = SocketAddr::from(([127, 0, 0, 1], 8000));
@@ -38,7 +49,25 @@ async fn health_check() -> StatusCode {
     StatusCode::OK
 }
 
-async fn subscribe(form: Form<FormData>) -> StatusCode {
+async fn subscribe(form: Form<FormData>, Extension(pool): Extension<Pool<Postgres>>) -> StatusCode {
     tracing::info!("name - {} | email - {}", form.0.name, form.0.email);
-    StatusCode::OK
+    match sqlx::query!(
+        r#"
+            INSERT INTO subscriptions (id, name, email, subscribed_at)
+            VALUES ($1, $2, $3, $4)
+        "#,
+        Uuid::new_v4(),
+        form.0.name,
+        form.0.email,
+        Utc::now()
+    )
+    .execute(&pool)
+    .await
+    {
+        Ok(_) => StatusCode::OK,
+        Err(e) => {
+            tracing::error!("{}", e);
+            StatusCode::INTERNAL_SERVER_ERROR
+        }
+    }
 }
