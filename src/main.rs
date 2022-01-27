5656716573269aa301c9b496a48173ba03555cdb
Sayan Mallick <snyxmk@gmail.com>
2022-01-27 11:55:53 +0530

    Fresh Setup Using Actix

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,14 @@
-use sqlx::postgres::PgPoolOptions;
-use std::net::TcpListener;
+use actix_web::{web, App, HttpRequest, HttpServer, Responder};
 
-use zero2prod::configuration::get_configuration;
-use zero2prod::startup::run;
-use zero2prod::telemetry::{get_subscriber, init_subscriber};
-
-// TODO - Add CI/CD support for Rust and Postgres
+async fn greet(req: HttpRequest) -> impl Responder {
+    let name = req.match_info().get("name").unwrap_or("World");
+    format!("Hello {}!", &name)
+}
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    let subscriber = get_subscriber("zero2prod".into(), "info".into(), std::io::stdout);
-    init_subscriber(subscriber);
-
-    let config = get_configuration().expect("Failed to load configuration");
-
-    let connection_pool = PgPoolOptions::new()
-        .connect_lazy(&config.database.connection_string())
-        .expect("Failed to connect to database");
-
-    let address = format!("{}:{}", config.application.host, config.application.port);
-
-    let listener = TcpListener::bind(address).expect("Failed to bind");
-
-    run(listener, connection_pool)?.await
+    HttpServer::new(|| App::new().route("/{name}", web::get().to(greet)))
+        .bind("127.0.0.1:8000")?
+        .run()
+        .await
 }
